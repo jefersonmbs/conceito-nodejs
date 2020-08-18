1b0dd0bc554fbd15a1ab3b8aa656292804061d67
Jeferson <jefersonmartinsbsantos@gmail.com>
2020-08-17 22:44:03 -0300

    Finalização

--- src/app.js
+++ src/app.js
@@ -1,7 +1,8 @@
+
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -11,23 +12,81 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+    return response.json(repositories)
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+    const { title, url, techs } = request.body
+
+    const repo = {
+        id: uuid(),
+        title,
+        url,
+        techs,
+        likes: 0
+    }
+    repositories.push(repo)
+
+    return response.json(repo)
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+    const { id } = request.params
+    const { title, url, techs } = request.body
+
+    const repoIndex = repositories.findIndex(repository => repository.id === id)
+
+    if(repoIndex < 0) {
+        return response.status(400).json({ error: "Id Não encontrado"})
+    }
+
+    const repo = {
+        ...repositories[repoIndex],
+        title,
+        url,
+        techs
+    }
+
+    repositories[repoIndex] = repo
+
+    return response.json(repo)
+
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+    const {id} = request.params
+
+    const repoIndex = repositories.findIndex(repository => repository.id === id)
+
+    if(repoIndex < 0) {
+        return response.status(400).json({ error: "ID não econtrado!"})
+    }
+
+    repositories.splice(repoIndex, 1)
+
+    return response.status(204).send()
+
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+    const { id } = request.params
+
+    const repoIndex = repositories.findIndex(repository => repository.id === id)
+
+    if(repoIndex < 0) {
+        return response.status(400).json({ error: "ID not found!"})
+    }
+
+    const { likes } = repositories[repoIndex]
+
+    const repository = {
+        ...repositories[repoIndex],
+        likes: likes + 1
+    }
+
+    repositories[repoIndex] = repository
+
+    return response.json(repository)
 });
 
 module.exports = app;
